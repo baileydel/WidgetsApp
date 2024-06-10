1b4d4ccbb6db853fc88152bb031e25ee4bb06134
Bailey <53024171+baileydel@users.noreply.github.com>
2024-06-10 09:01:16 -0700

    Add project files.

---
+++ WidgetsApp/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WidgetsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackColor = Color.LimeGreen;
+            this.TransparencyKey = BackColor;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Panel pane = new ResizablePanel();
+            this.Controls.Add(pane);
+        }
+    }
+}
---
+++ WidgetsApp/ResizablePanel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WidgetsApp
+{
+    internal class ResizablePanel : Panel
+    {
+
+        public ResizablePanel()
+        {
+            this.BackColor = Color.Black;
+            this.Size = new Size(600, 400);
+        }
+    }
+}
