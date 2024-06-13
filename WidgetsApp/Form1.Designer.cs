b66eabdf731c70759da4e92ac45eae7d17a795ae
Bailey <53024171+baileydel@users.noreply.github.com>
2024-06-13 15:32:48 -0700

    Update Form1.Designer.cs
    Removed FormClosed Event

--- WidgetsApp/Form1.Designer.cs
+++ WidgetsApp/Form1.Designer.cs
@@ -84,7 +84,6 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.TransparencyKey = System.Drawing.Color.Lime;
-            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
