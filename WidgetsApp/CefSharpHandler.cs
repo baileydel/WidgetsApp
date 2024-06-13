0d2d89947ba7e8bfb7f2da2103790b85e0907750
Bailey <53024171+baileydel@users.noreply.github.com>
2024-06-13 15:33:38 -0700

    Create CefSharpHandler.cs
    Allows browser to call this class methods

---
+++ WidgetsApp/CefSharpHandler.cs
@@ -0,0 +1,23 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System.Threading.Tasks;
+
+namespace WidgetsApp
+{
+    public class CefSharpHandler
+    {
+        private readonly ChromiumWebBrowser browser;
+
+        public CefSharpHandler(ChromiumWebBrowser browser)
+        {
+            this.browser = browser;
+        }
+
+        // Method to handle PostMessage from JavaScript
+        public async Task<bool> Input(string input)
+        {
+            await browser.GetDevToolsClient()?.Input?.InsertTextAsync(input);
+            return true;
+        }
+    }
+}
