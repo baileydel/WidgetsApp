7b8f85a64545d5d0b0c9b459a8630129e6aee426
Bailey <53024171+baileydel@users.noreply.github.com>
2024-06-20 00:12:02 -0700

    Update ResizablePanel.cs

--- WidgetsApp/src/controls/ResizablePanel.cs
+++ WidgetsApp/src/controls/ResizablePanel.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Drawing;
-using System.Linq.Expressions;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace WidgetsApp
 {
     internal class ResizablePanel : Panel
     {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,     // x-coordinate of upper-left corner
+            int nTopRect,      // y-coordinate of upper-left corner
+            int nRightRect,    // x-coordinate of lower-right corner
+            int nBottomRect,   // y-coordinate of lower-right corner
+            int nWidthEllipse, // height of ellipse
+            int nHeightEllipse // width of ellipse
+        );
+
         bool Dragging = false;
         Point DragStart = Point.Empty;
 
@@ -22,121 +33,111 @@ namespace WidgetsApp
 
         public bool Editable = true;
 
-
         public ResizablePanel()
         {
             this.SuspendLayout();
             this.BackColor = Color.Black;
             this.ForeColor = SystemColors.ControlLightLight;
             this.Size = new Size(600, 400);
+            this.BorderStyle = BorderStyle.None;
+            Region = Region.FromHrgn(CreateRoundRectRgn(this.Left, this.Top, this.Width, this.Height, 20, 20));
             this.ResumeLayout(false);
 
-            this.MouseDown += delegate (object sender, MouseEventArgs e)
+            this.MouseDown += panel_MouseDown;
+            this.MouseUp += panel_MouseUp;
+            this.MouseMove += panel_MouseMove;
+        }
+
+        private void panel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (MouseIsInLeftEdge || MouseIsInRightEdge || MouseIsInTopEdge || MouseIsInBottomEdge)
             {
-                if (MouseIsInLeftEdge || MouseIsInRightEdge || MouseIsInTopEdge || MouseIsInBottomEdge)
-                {
-                    Resizing = true;
-                    CursorStartPoint = new Point(e.X, e.Y);
-                    CurrentStartSize = this.Size;
-                }
-                else
-                {
-                    Dragging = true;
-                    DragStart = new Point(e.X, e.Y);
-                }
-            };
-
-            this.MouseUp += delegate (object sender, MouseEventArgs e)
+                Resizing = true;
+                CursorStartPoint = new Point(e.X, e.Y);
+                CurrentStartSize = this.Size;
+            }
+            else
             {
-                Dragging = false;
-                Resizing = false;
-            };
+                Dragging = true;
+                DragStart = new Point(e.X, e.Y);
+            }
+        }
 
-            this.MouseMove += delegate (object sender, MouseEventArgs e)
+        private void panel_MouseUp(object sender, MouseEventArgs e)
+        {
+            Dragging = false;
+            Resizing = false;
+        }
+
+        private void panel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!Editable)
             {
-                if (!Editable)
-                {
-                    return;
-                }
+                return;
+            }
 
-                if (!Dragging && !Resizing)
-                {
-                    MouseIsInLeftEdge = Math.Abs(e.X) <= 5;
-                    MouseIsInRightEdge = Math.Abs(e.X - this.Width) <= 5;
-                    MouseIsInTopEdge = Math.Abs(e.Y) <= 5;
-                    MouseIsInBottomEdge = Math.Abs(e.Y - this.Height) <= 5;
-                }
+            if (!Dragging && !Resizing)
+            {
+                MouseIsInLeftEdge = Math.Abs(e.X) <= 5;
+                MouseIsInRightEdge = Math.Abs(e.X - this.Width) <= 5;
+                MouseIsInTopEdge = Math.Abs(e.Y) <= 5;
+                MouseIsInBottomEdge = Math.Abs(e.Y - this.Height) <= 5;
+            }
 
-                if (Resizing)
-                {
-                    if (MouseIsInLeftEdge)
-                    {
-                        this.Width -= (e.X - CursorStartPoint.X);
-                        this.Left += (e.X - CursorStartPoint.X);
-                    }
-                    else if (MouseIsInRightEdge)
-                    {
-                        this.Width = (e.X - CursorStartPoint.X) + CurrentStartSize.Width;
-                    }
-
-                    if (MouseIsInTopEdge)
-                    {
-                        this.Height -= (e.Y - CursorStartPoint.Y);
-                        this.Top += (e.Y - CursorStartPoint.Y);
-                    }
-                    else if (MouseIsInBottomEdge)
-                    {
-                        this.Height = (e.Y - CursorStartPoint.Y)  + CurrentStartSize.Height;
-                    }
-                }
-
-
+            if (Resizing)
+            {
                 if (MouseIsInLeftEdge)
                 {
-                    if (MouseIsInTopEdge)
-                    {
-                        this.Cursor = Cursors.SizeNWSE;
-                    }
-                    else if (MouseIsInBottomEdge)
-                    {
-                        this.Cursor = Cursors.SizeNESW;
-                    }
-                    else
-                    {
-                        this.Cursor = Cursors.SizeWE;
-                    }
+                    this.Width -= (e.X - CursorStartPoint.X);
+                    this.Left += (e.X - CursorStartPoint.X);
                 }
                 else if (MouseIsInRightEdge)
                 {
-                    if (MouseIsInTopEdge)
-                    {
-                        this.Cursor = Cursors.SizeNESW;
-                    }
-                    else if (MouseIsInBottomEdge)
-                    {
-                        this.Cursor = Cursors.SizeNWSE;
-                    }
-                    else
-                    {
-                        this.Cursor = Cursors.SizeWE;
-                    }
-
+                    this.Width = (e.X - CursorStartPoint.X) + CurrentStartSize.Width;
                 }
-                else if (MouseIsInTopEdge || MouseIsInBottomEdge)
+
+                if (MouseIsInTopEdge)
                 {
-                    this.Cursor = Cursors.SizeNS;
+                    this.Height -= (e.Y - CursorStartPoint.Y);
+                    this.Top += (e.Y - CursorStartPoint.Y);
+                }
+                else if (MouseIsInBottomEdge)
+                {
+                    this.Height = (e.Y - CursorStartPoint.Y) + CurrentStartSize.Height;
+                }
+                Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+            }
+
+
+            if (MouseIsInLeftEdge || MouseIsInRightEdge)
+            {
+                if (MouseIsInTopEdge)
+                {
+                    this.Cursor = MouseIsInLeftEdge ? Cursors.SizeNWSE : Cursors.SizeNESW;
+                }
+                else if (MouseIsInBottomEdge)
+                {
+                    this.Cursor = MouseIsInLeftEdge ? Cursors.SizeNESW : Cursors.SizeNWSE;
                 }
                 else
                 {
-                    this.Cursor = Cursors.Default;
+                    this.Cursor = Cursors.SizeWE;
                 }
+            }
+            else if (MouseIsInTopEdge || MouseIsInBottomEdge)
+            {
+                this.Cursor = Cursors.SizeNS;
+            }
+            else
+            {
+                this.Cursor = Cursors.Default;
+            }
 
-                if (Dragging && !Resizing)
-                {
-                    this.Left = Math.Max(0, e.X + this.Left - DragStart.X);
-                    this.Top = Math.Max(0, e.Y + this.Top - DragStart.Y);
-                }
-            };
+            if (Dragging && !Resizing)
+            {
+                this.Left = Math.Max(0, e.X + this.Left - DragStart.X);
+                this.Top = Math.Max(0, e.Y + this.Top - DragStart.Y);
+            }
         }
     }
 }
