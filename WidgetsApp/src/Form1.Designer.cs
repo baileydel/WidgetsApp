52d0a3b24a9b2e95f224fdb97889b3126d4c8f4b
Bailey <53024171+baileydel@users.noreply.github.com>
2024-06-28 21:33:21 -0700

    Update Form1.Designer.cs

--- WidgetsApp/src/Form1.Designer.cs
+++ WidgetsApp/src/Form1.Designer.cs
@@ -49,7 +49,7 @@
             this.panel1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.panel1.Location = new System.Drawing.Point(437, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(662, 76);
+            this.panel1.Size = new System.Drawing.Size(662, 74);
             this.panel1.TabIndex = 0;
             // 
             // button4
