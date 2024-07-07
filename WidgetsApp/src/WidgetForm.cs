1fabef0e26aa6dee0770676cead4d86d855ab015
Bailey <53024171+baileydel@users.noreply.github.com>
2024-07-07 00:11:46 -0700

    WidgetForm Development
    Starting Development on using Forms instead of Panels.

---
+++ WidgetsApp/src/WidgetForm.cs
@@ -0,0 +1,16 @@
+﻿using CefSharp.WinForms;
+using System.Windows.Forms;
+
+namespace WidgetsApp
+{
+    public partial class WidgetForm : Form
+    {
+        public WidgetForm()
+        {
+            InitializeComponent();
+
+            ChromiumWebBrowser browser = new ChromiumWebBrowser("https://www.google.com");
+            this.Controls.Add(browser);
+        }
+    }
+}
