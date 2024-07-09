9beb6fefb9a4b931d4be7e6a2c0f05dc4c3b4838
Bailey <53024171+baileydel@users.noreply.github.com>
2024-07-09 14:32:50 -0700

    Moved to src/controls

---
+++ WidgetsApp/src/controls/ShortcutControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WidgetsApp.src.controls
+{
+    public partial class ShortcutControl : UserControl
+    {
+        public ShortcutControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
