7a3a5f4ec99c4292a407b02226c45ea82e425015
Bailey <53024171+baileydel@users.noreply.github.com>
2024-07-08 21:06:13 -0700

    Create WidgetData.cs

---
+++ WidgetsApp/src/WidgetData.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+namespace WidgetsApp
+{
+    public partial class WidgetData
+    {
+        public Point location;
+        public Size size;
+        public string url;
+
+        public WidgetData(Size size, Point location, string url)
+        {
+            this.size = size;
+            this.location = location;
+            this.url = url;
+        }
+    }
+}
