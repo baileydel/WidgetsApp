3ad4508a198d24d97263d0955cd7b4bc49b85137
Bailey <53024171+baileydel@users.noreply.github.com>
2024-07-08 21:05:58 -0700

    Renamed Form1 -> MainForm

---
+++ WidgetsApp/src/MainForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿using System.Drawing;
+
+namespace WidgetsApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(64, 63);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.Location = new System.Drawing.Point(643, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(73, 63);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.ClientSize = new System.Drawing.Size(728, 87);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Name = "MainForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ WidgetsApp/src/MainForm.cs
@@ -0,0 +1,77 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WidgetsApp
+{
+    public partial class MainForm : Form
+    {
+        public static readonly string PATH = AppDomain.CurrentDomain.BaseDirectory;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeCefSharp();
+            LoadPrevious();
+        }
+
+        private void InitializeCefSharp()
+        {
+            CefSettingsBase settings = new CefSettings
+            {
+                CachePath = PATH + @"\browser"
+            };
+
+            CefSharpSettings.ConcurrentTaskExecution = true;
+            CefSharpSettings.ShutdownOnExit = true;
+
+            settings.CefCommandLineArgs.Add("enable-persistent-cookies", "1");
+
+            Cef.Initialize(settings);
+        }
+
+        private void LoadPrevious()
+        {
+            string savepath = PATH + @"\save";
+            string[] files = Directory.GetFiles(savepath);
+
+            foreach (string file in files)
+            {
+                string json = File.ReadAllText(file);
+                WidgetData data = JsonConvert.DeserializeObject<WidgetData>(json);
+
+                CreateChild(data);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+        
+            CreateChild(null);
+        }
+
+
+        private void CreateChild(WidgetData data)
+        {
+            WidgetForm form = new WidgetForm(data);
+            form.Show();
+        }
+
+        private void import(object sender, EventArgs e)
+        {
+            DialogResult r = openFileDialog1.ShowDialog();
+
+            if (r == DialogResult.OK)
+            {
+                openFileDialog1.CheckFileExists = true;
+                string json = File.ReadAllText(openFileDialog1.FileName);
+                WidgetData data = JsonConvert.DeserializeObject<WidgetData>(json);
+
+                File.Delete(openFileDialog1.FileName);
+            }
+        }
+    }
+}
