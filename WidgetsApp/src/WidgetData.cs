8cb12d8c4df8964db0512f1cf2f0918c5d1d50f6
Bailey <53024171+baileydel@users.noreply.github.com>
2024-07-09 14:30:58 -0700

    Refactor: Update WidgetData class to use properties and add new constructors
    - Changed WidgetData class from partial to a complete class definition.
    - Replaced public fields with auto-implemented properties for Name, Location, Size, and Url.
    - Modified the existing constructor to initialize properties instead of fields.
    - Added new constructors to support different initialization scenarios.

--- WidgetsApp/src/WidgetData.cs
+++ WidgetsApp/src/WidgetData.cs
@@ -1,18 +1,37 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 
 namespace WidgetsApp
 {
-    public partial class WidgetData
+    public class WidgetData
     {
-        public Point location;
-        public Size size;
-        public string url;
+        public string Name { get; set; }
+        public Point Location { get; set; }
+        public Size Size { get; set; }
+        public string Url { get; set; }
 
         public WidgetData(Size size, Point location, string url)
         {
-            this.size = size;
-            this.location = location;
-            this.url = url;
+            Name = url; 
+            Size = size;
+            Location = location;
+            Url = url;
+        }
+
+        public WidgetData(string name, Size size, Point location, string url)
+        {
+            Name = name;
+            Size = size;
+            Location = location;
+            Url = url;
+        }
+
+        public WidgetData(string name, string url)
+        {
+            Name = name;
+            Url = url;
+            Size = new Size(526, 337);
+            Location = new Point(0, 0);
         }
     }
 }
