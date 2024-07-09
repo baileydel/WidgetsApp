c15849fc93b4faca63814670e5b062b292769ed3
Bailey <53024171+baileydel@users.noreply.github.com>
2024-07-09 14:14:05 -0700

    Create ShortcutForm.Designer.cs

---
+++ WidgetsApp/src/ShortcutForm.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace WidgetsApp
+{
+    partial class UserControl1
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.UrlTextBox = new System.Windows.Forms.TextBox();
+            this.urlLabel = new System.Windows.Forms.Label();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.DoneButton = new System.Windows.Forms.Button();
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.ForeColor = System.Drawing.Color.White;
+            this.NameLabel.Location = new System.Drawing.Point(22, 59);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(35, 13);
+            this.NameLabel.TabIndex = 0;
+            this.NameLabel.Text = "Name";
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(83)))), ((int)(((byte)(83)))));
+            this.NameTextBox.ForeColor = System.Drawing.Color.White;
+            this.NameTextBox.Location = new System.Drawing.Point(25, 75);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(483, 20);
+            this.NameTextBox.TabIndex = 1;
+            // 
+            // UrlTextBox
+            // 
+            this.UrlTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(83)))), ((int)(((byte)(83)))));
+            this.UrlTextBox.ForeColor = System.Drawing.Color.White;
+            this.UrlTextBox.Location = new System.Drawing.Point(25, 132);
+            this.UrlTextBox.Name = "UrlTextBox";
+            this.UrlTextBox.Size = new System.Drawing.Size(483, 20);
+            this.UrlTextBox.TabIndex = 3;
+            this.UrlTextBox.TextChanged += new System.EventHandler(this.urlBox_TextChanged);
+            this.UrlTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.urlBox_KeyPress);
+            // 
+            // urlLabel
+            // 
+            this.urlLabel.AutoSize = true;
+            this.urlLabel.ForeColor = System.Drawing.Color.White;
+            this.urlLabel.Location = new System.Drawing.Point(22, 116);
+            this.urlLabel.Name = "urlLabel";
+            this.urlLabel.Size = new System.Drawing.Size(29, 13);
+            this.urlLabel.TabIndex = 2;
+            this.urlLabel.Text = "URL";
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.CancelButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CancelButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(190)))), ((int)(((byte)(238)))));
+            this.CancelButton.Location = new System.Drawing.Point(352, 200);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(75, 34);
+            this.CancelButton.TabIndex = 4;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = false;
+            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // DoneButton
+            // 
+            this.DoneButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(42)))), ((int)(((byte)(42)))));
+            this.DoneButton.Enabled = false;
+            this.DoneButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.DoneButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DoneButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(112)))), ((int)(((byte)(112)))));
+            this.DoneButton.Location = new System.Drawing.Point(433, 200);
+            this.DoneButton.Name = "DoneButton";
+            this.DoneButton.Size = new System.Drawing.Size(75, 34);
+            this.DoneButton.TabIndex = 5;
+            this.DoneButton.Text = "Done";
+            this.DoneButton.UseVisualStyleBackColor = false;
+            this.DoneButton.Click += new System.EventHandler(this.DoneButton_Click);
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.TitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitleLabel.ForeColor = System.Drawing.Color.White;
+            this.TitleLabel.Location = new System.Drawing.Point(21, 24);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(103, 20);
+            this.TitleLabel.TabIndex = 6;
+            this.TitleLabel.Text = "Add Shortcut";
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.Controls.Add(this.TitleLabel);
+            this.Controls.Add(this.DoneButton);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.UrlTextBox);
+            this.Controls.Add(this.urlLabel);
+            this.Controls.Add(this.NameTextBox);
+            this.Controls.Add(this.NameLabel);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(525, 254);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.TextBox UrlTextBox;
+        private System.Windows.Forms.Label urlLabel;
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.Button DoneButton;
+        private System.Windows.Forms.Label TitleLabel;
+    }
+}
