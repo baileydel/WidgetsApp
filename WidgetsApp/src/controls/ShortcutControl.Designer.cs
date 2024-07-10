a067d6dc7e767b02b8bcf43cf22c78ec76a10f89
Bailey <53024171+baileydel@users.noreply.github.com>
2024-07-10 10:58:54 -0700

    Created: ShortcutControl

--- WidgetsApp/src/controls/ShortcutControl.Designer.cs
+++ WidgetsApp/src/controls/ShortcutControl.Designer.cs
@@ -28,10 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.components = new System.ComponentModel.Container();
+            this.SettingsButton = new System.Windows.Forms.Button();
+            this.ShowTimer = new System.Windows.Forms.Timer(this.components);
+            this.HideTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // SettingsButton
+            // 
+            this.SettingsButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.SettingsButton.BackColor = System.Drawing.Color.Transparent;
+            this.SettingsButton.FlatAppearance.BorderSize = 0;
+            this.SettingsButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(70)))), ((int)(((byte)(70)))));
+            this.SettingsButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.SettingsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SettingsButton.Font = new System.Drawing.Font("Minecraft", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SettingsButton.Location = new System.Drawing.Point(177, 3);
+            this.SettingsButton.Name = "SettingsButton";
+            this.SettingsButton.Size = new System.Drawing.Size(31, 29);
+            this.SettingsButton.TabIndex = 2;
+            this.SettingsButton.Text = ":";
+            this.SettingsButton.UseVisualStyleBackColor = false;
+            this.SettingsButton.Visible = false;
+            this.SettingsButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ShowTimer
+            // 
+            this.ShowTimer.Interval = 1000;
+            this.ShowTimer.Tick += new System.EventHandler(this.ShowTimer_Tick);
+            // 
+            // HideTimer
+            // 
+            this.HideTimer.Tick += new System.EventHandler(this.HideTimer_Tick);
+            // 
+            // ShortcutControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.Controls.Add(this.SettingsButton);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "ShortcutControl";
+            this.Size = new System.Drawing.Size(211, 246);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Button SettingsButton;
+        private System.Windows.Forms.Timer ShowTimer;
+        private System.Windows.Forms.Timer HideTimer;
     }
 }
--- WidgetsApp/src/controls/ShortcutControl.cs
+++ WidgetsApp/src/controls/ShortcutControl.cs
@@ -1,20 +1,147 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing.Drawing2D;
+
 using System.Windows.Forms;
 
 namespace WidgetsApp.src.controls
 {
     public partial class ShortcutControl : UserControl
     {
+        public Color OuterColor;
+        public Color InnerColor;
+        private bool Elapsed;
+
+        public string InnerText;
+        public string OuterText;
+
+        public Color InnerTextColor;
+
+        public int InnerFontSize;
+
+        public int state;
+
         public ShortcutControl()
         {
             InitializeComponent();
+
+            OuterText = "Outer: ";
+            InnerText = OuterText[0].ToString().ToUpper();
+
+            InnerTextColor = Color.Black;
+
+            InnerFontSize = 12;
+
+            Width = 120;
+            Height = 120;
+
+            Elapsed = false;
+            state = 0;
+
+            MouseEnter += (sender, e) => Hover_Event();
+            MouseLeave += (sender, e) => Leave_Event();
+
+            SettingsButton.MouseEnter += (sender, e) => Hover_Event();
+
+            Random random = new Random();
+            InnerColor = Color.FromArgb(random.Next(150, 256), random.Next(150, 256), random.Next(150, 256));
+            OuterColor = Color.FromArgb(40, 40, 40);
+        }   
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            graphics.SmoothingMode = SmoothingMode.HighQuality;
+
+            int dimension = Math.Min(this.Width, this.Height); // Use the smaller dimension
+            int outerCircleSize = (int)(dimension * 0.5);
+
+            int outerCircleX = (this.Width - outerCircleSize) / 2;
+            int centerYOffset = Math.Max(0, (this.Height - dimension) / 2) - dimension / 8; // Offset both circles up
+            int outerCircleY = centerYOffset + dimension / 4;
+
+            using (SolidBrush outerBrush = new SolidBrush(OuterColor))
+            {
+                graphics.FillEllipse(outerBrush, outerCircleX, outerCircleY, outerCircleSize, outerCircleSize);
+            }
+
+            int innerCircleSize = outerCircleSize * 2 / 3;
+            int innerCircleX = outerCircleX + (outerCircleSize - innerCircleSize) / 2;
+            int innerCircleY = outerCircleY + (outerCircleSize - innerCircleSize) / 2;
+
+            using (SolidBrush innerBrush = new SolidBrush(InnerColor))
+            {
+                graphics.FillEllipse(innerBrush, innerCircleX, innerCircleY, innerCircleSize, innerCircleSize);
+            }
+
+            Font innerFont = new Font("Arial", InnerFontSize);
+
+            SizeF innerTextSize = graphics.MeasureString(InnerText, innerFont);
+            Brush innerTextBrush = new SolidBrush(InnerTextColor);
+
+            float innerTextX = innerCircleX + (innerCircleSize - innerTextSize.Width) / 2;
+            float innerTextY = innerCircleY + (innerCircleSize - innerTextSize.Height) / 2 + (InnerFontSize * 0.1f);
+
+            graphics.DrawString(InnerText, innerFont, innerTextBrush, innerTextX, innerTextY);
+
+
+            float outerFontSize = Math.Max(this.Width, this.Height) * 0.06f; // Adjust the multiplier for desired font size
+            Font outerFont = new Font("Arial", outerFontSize);
+            SizeF outerTextSize = graphics.MeasureString(OuterText, outerFont);
+            Brush outerTextBrush = new SolidBrush(Color.White);
+
+            float outerTextX = outerCircleX + (outerCircleSize - outerTextSize.Width) / 2;
+            float outerTextY = outerCircleY + outerCircleSize + outerCircleSize / 5; // Increase the offset
+
+            graphics.DrawString(OuterText, outerFont, outerTextBrush, outerTextX, outerTextY);
+        }
+
+        private void Leave_Event()
+        {
+            this.BackColor = Color.FromArgb(32, 32, 32);
+            Elapsed = false;
+
+            HideTimer.Start();
+            ShowTimer.Stop();
+        }
+
+        private void Hover_Event()
+        {
+            this.BackColor = Color.FromArgb(50, 50, 50);
+
+            if (HideTimer.Enabled)
+            {
+                HideTimer.Stop();
+            }
+
+            if (state == 0)
+            {
+                if (Elapsed)
+                {
+                    SettingsButton.Show();
+                }
+                else
+                {
+                    ShowTimer.Start();
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // create dropdown menu
+            Console.WriteLine("Button clicked");
+        }
+
+        private void ShowTimer_Tick(object sender, EventArgs e)
+        {
+            SettingsButton.Show();
+        }
+
+        private void HideTimer_Tick(object sender, EventArgs e)
+        {
+            SettingsButton.Hide();
         }
     }
 }
