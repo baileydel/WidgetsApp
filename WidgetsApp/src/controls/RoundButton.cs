b1975de3e3f3231900f83b6c25afca47cf694c61
Bailey <53024171+baileydel@users.noreply.github.com>
2024-07-12 09:09:14 -0700

    Create RoundButton.cs

---
+++ WidgetsApp/src/controls/RoundButton.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+using System.Windows.Forms;
+
+namespace WidgetsApp.src.controls
+{
+    public class RoundButton : Button
+    {
+        private int borderSize = 0;
+        private int borderRadius = 40;
+        private Color borderColor = Color.FromArgb(6, 46, 111);
+
+        public int BorderSize { get => borderSize; set { borderSize = value; this.Invalidate(); } }
+        public int BorderRadius { get => borderRadius; set { borderRadius = value; this.Invalidate(); } }
+        public Color BorderColor { get => borderColor; set { borderColor = value; this.Invalidate(); } }
+
+        public RoundButton() { 
+            this.FlatStyle = FlatStyle.Flat;
+            this.FlatAppearance.BorderSize = 0;
+            this.Size = new Size(150, 40);
+            this.BackColor = Color.FromArgb(160, 189, 237);
+            this.ForeColor = Color.FromArgb(6, 46, 111);
+        }
+
+        private GraphicsPath GetFigurePath(RectangleF rect, float radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            path.StartFigure();
+            path.AddArc(rect.X, rect.Y, radius, radius, 180, 90);
+            path.AddArc(rect.Width - radius, rect.Y, radius, radius, 270, 90);
+            path.AddArc(rect.Width - radius, rect.Height - radius, radius, radius, 0, 90);
+            path.AddArc(rect.X, rect.Height - radius, radius, radius, 90, 90);
+            path.CloseFigure();
+            return path;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics graphics = e.Graphics;
+            graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            RectangleF rectSurface =  new RectangleF(0, 0, this.Width, this.Height);
+            RectangleF rectBorder =  new RectangleF(1, 1, this.Width - 0.8F, this.Height - 1);
+
+            //Round the borders
+            if (borderRadius > 2)
+            {
+                using (GraphicsPath pathSurface = GetFigurePath(rectSurface, borderRadius))
+                using (GraphicsPath pathBorder = GetFigurePath(rectBorder, borderRadius - 1))
+                using (Pen penSurface = new Pen(this.Parent.BackColor, 2))
+                using (Pen penBorder = new Pen(borderColor, borderSize))
+                {
+                    penBorder.Alignment = PenAlignment.Inset;
+                    this.Region = new Region(pathSurface);
+                    graphics.DrawPath(penSurface, pathSurface);
+
+                    if (borderSize >= 1)
+                    {
+                        graphics.DrawPath(penBorder, pathBorder);
+                    }
+                }
+            }
+            else
+            {
+                this.Region = new Region(rectSurface);
+                if (borderSize >= 1)
+                {
+                    using  (Pen penBorder = new Pen(borderColor, borderSize))
+                    {
+                        penBorder.Alignment = PenAlignment.Inset;
+                        graphics.DrawRectangle(penBorder, 0, 0, this.Width - 1, this.Height - 1);
+                    }
+                }
+            }
+        }
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            this.Parent.BackColorChanged += new EventHandler((source, args) => { 
+                if  (this.DesignMode)
+                {
+                    this.Invalidate();
+                }
+            });
+        }
+    }
+}
