d676fc4df7e42ee889c051b358028aeca2f0db2a
Bailey <53024171+baileydel@users.noreply.github.com>
2024-07-12 09:46:52 -0700

    Refactor: MainForm.cs
    Removed unused method import
    Added EditShortcut
    Added SaveShortcut
    Added DeleteShortcut

--- WidgetsApp/src/MainForm.cs
+++ WidgetsApp/src/MainForm.cs
@@ -21,17 +21,16 @@ namespace WidgetsApp
             MakePaths();
             InitializeComponent();
 
-            shortcutControl2.state = 1;
-            shortcutControl2.OuterText = "Add shortcut";
-            shortcutControl2.InnerText = "+";
-            shortcutControl2.MouseClick += ShortcutButton_Click;
+            AddShortcutControl.state = 1;
+            AddShortcutControl.OuterText = "Add shortcut";
+            AddShortcutControl.InnerText = "+";
+            AddShortcutControl.MouseClick += ShortcutButton_Click;
 
-            shortcutControl2.OuterColor = Color.FromArgb(0, 74, 119);
-            shortcutControl2.InnerColor = Color.FromArgb(0, 74, 119);    
+            AddShortcutControl.OuterColor = Color.FromArgb(0, 74, 119);
+            AddShortcutControl.InnerColor = Color.FromArgb(0, 74, 119);    
 
-            shortcutControl2.InnerTextColor = Color.White;
-            shortcutControl2.InnerFontSize = 24;
-
+            AddShortcutControl.InnerTextColor = Color.White;
+            AddShortcutControl.InnerFontSize = 24;
 
             InitializeCefSharp();
             LoadPrevious();
@@ -44,11 +43,10 @@ namespace WidgetsApp
                 CachePath = BROWSERPATH
             };
 
-            CefSharpSettings.ConcurrentTaskExecution = true;
-            CefSharpSettings.ShutdownOnExit = true;
-
             settings.CefCommandLineArgs.Add("enable-persistent-cookies", "1");
 
+            CefSharpSettings.ConcurrentTaskExecution = true;
+            CefSharpSettings.ShutdownOnExit = true;
             Cef.Initialize(settings);
         }
 
@@ -83,10 +81,15 @@ namespace WidgetsApp
 
         private void ShortcutButton_Click(object sender, EventArgs e)
         {
-            flowLayoutPanel1.Hide();
+            FlowPanel.Hide();
+            UserControl shortcut = new ShortcutForm();
+            Controls.Add(shortcut);
+        }
 
-            UserControl shortuct = new ShortcutForm();
-            this.Controls.Add(shortuct);
+        public void CreateShortcut(WidgetData data)
+        {
+            AddShortcut(data);
+            SaveShortcut(data, null);
         }
 
         public void AddShortcut(WidgetData data)
@@ -94,14 +97,47 @@ namespace WidgetsApp
             ShortcutControl control = new ShortcutControl()
             {
                 OuterText = data.Name,
+                InnerText = data.Url,
             };
 
             control.MouseClick += (sender, e) => LaunchShortcut(data);
       
-            flowLayoutPanel1.Controls.Add(control);
-            flowLayoutPanel1.Controls.SetChildIndex(shortcutControl2, flowLayoutPanel1.Controls.Count - 1);
+            FlowPanel.Controls.Add(control);
+            FlowPanel.Controls.SetChildIndex(AddShortcutControl, FlowPanel.Controls.Count - 1);
         }
 
+        public void EditShortcut(ShortcutControl shortcutControl)
+        {
+            FlowPanel.Hide();
+            UserControl shortcut = new ShortcutForm(shortcutControl);
+            Controls.Add(shortcut);
+        }
+
+        public void RemoveShortcut(ShortcutControl control)
+        {
+            FlowPanel.Controls.Remove(control);
+            DeleteShortcut(control);
+        }
+
+        public void DeleteShortcut(ShortcutControl control)
+        {
+            if (File.Exists(SAVEPATH + @"\" + control.OuterText + ".json"))
+            {
+                File.Delete(SAVEPATH + @"\" + control.OuterText + ".json");
+            }
+        }
+
+        public void SaveShortcut(WidgetData data, ShortcutControl edit)
+        {
+            if (edit != null)
+            {
+                DeleteShortcut(edit);
+            }
+
+            string json = JsonConvert.SerializeObject(data);
+            File.WriteAllText(SAVEPATH + @"\" + data.Name + ".json", json);
+        }
+ 
         public void LaunchShortcut(WidgetData data)
         {
             Console.WriteLine("Launching shortcut");
@@ -113,30 +149,12 @@ namespace WidgetsApp
         {
             if (b)
             {
-                flowLayoutPanel1.Hide();
+                FlowPanel.Hide();
             }
             else
             {
-                flowLayoutPanel1.Show();
+                FlowPanel.Show();
             }
         }
-
-        private void import(object sender, EventArgs e)
-        {
-            /**
-             * 
-            DialogResult r = openFileDialog1.ShowDialog();
-
-            if (r == DialogResult.OK)
-            {
-                openFileDialog1.CheckFileExists = true;
-                string json = File.ReadAllText(openFileDialog1.FileName);
-                WidgetData data = JsonConvert.DeserializeObject<WidgetData>(json);
-
-                File.Delete(openFileDialog1.FileName);
-            }
-            */
-        }
-
     }
 }
