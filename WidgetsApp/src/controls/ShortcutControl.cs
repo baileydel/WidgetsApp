504a42b73dcd43f64ddd6124de9342b9a3bba6be
Bailey <53024171+baileydel@users.noreply.github.com>
2024-07-12 09:44:15 -0700

    Refactor ShortcutControl.cs
    OnPaint() ~ Disposed of Fonts and Brushes properly.

--- WidgetsApp/src/controls/ShortcutControl.cs
+++ WidgetsApp/src/controls/ShortcutControl.cs
@@ -2,7 +2,7 @@
 
 using System.Drawing;
 using System.Drawing.Drawing2D;
-
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace WidgetsApp.src.controls
@@ -11,30 +11,28 @@ namespace WidgetsApp.src.controls
     {
         public Color OuterColor;
         public Color InnerColor;
-        private bool Elapsed;
 
-        public string InnerText;
-        public string OuterText;
+        private string innerText;
+        private string outerText;
 
         public Color InnerTextColor;
-
         public int InnerFontSize;
 
         public int state;
+        private bool Elapsed;
+
+        public string InnerText { get => innerText; set => innerText = value; }
+        public string OuterText { get => outerText; set => outerText = value; }
 
         public ShortcutControl()
         {
             InitializeComponent();
 
-            OuterText = "Outer: ";
-            InnerText = OuterText[0].ToString().ToUpper();
+            OuterText = "Name";
+            InnerText = "Url";
 
             InnerTextColor = Color.Black;
-
             InnerFontSize = 12;
-
-            Width = 120;
-            Height = 120;
 
             Elapsed = false;
             state = 0;
@@ -47,18 +45,18 @@ namespace WidgetsApp.src.controls
             Random random = new Random();
             InnerColor = Color.FromArgb(random.Next(150, 256), random.Next(150, 256), random.Next(150, 256));
             OuterColor = Color.FromArgb(40, 40, 40);
-        }   
+        }
 
         protected override void OnPaint(PaintEventArgs e)
         {
             Graphics graphics = e.Graphics;
             graphics.SmoothingMode = SmoothingMode.HighQuality;
 
-            int dimension = Math.Min(this.Width, this.Height); // Use the smaller dimension
+            int dimension = Math.Min(this.Width, this.Height);
             int outerCircleSize = (int)(dimension * 0.5);
 
             int outerCircleX = (this.Width - outerCircleSize) / 2;
-            int centerYOffset = Math.Max(0, (this.Height - dimension) / 2) - dimension / 8; // Offset both circles up
+            int centerYOffset = Math.Max(0, (this.Height - dimension) / 2) - dimension / 8;
             int outerCircleY = centerYOffset + dimension / 4;
 
             using (SolidBrush outerBrush = new SolidBrush(OuterColor))
@@ -75,31 +73,40 @@ namespace WidgetsApp.src.controls
                 graphics.FillEllipse(innerBrush, innerCircleX, innerCircleY, innerCircleSize, innerCircleSize);
             }
 
-            Font innerFont = new Font("Arial", InnerFontSize);
+            using (Font innerFont = new Font("Arial", InnerFontSize))
+            {
+                string inner = InnerText[0].ToString().ToUpper();
 
-            SizeF innerTextSize = graphics.MeasureString(InnerText, innerFont);
-            Brush innerTextBrush = new SolidBrush(InnerTextColor);
+                SizeF innerTextSize = graphics.MeasureString(inner, innerFont);
+                using (Brush innerTextBrush = new SolidBrush(InnerTextColor))
+                {
+                    float innerTextX = innerCircleX + (innerCircleSize - innerTextSize.Width) / 2;
+                    float innerTextY = innerCircleY + (innerCircleSize - innerTextSize.Height) / 2 + (InnerFontSize * 0.1f);
 
-            float innerTextX = innerCircleX + (innerCircleSize - innerTextSize.Width) / 2;
-            float innerTextY = innerCircleY + (innerCircleSize - innerTextSize.Height) / 2 + (InnerFontSize * 0.1f);
+                    graphics.DrawString(inner, innerFont, innerTextBrush, innerTextX, innerTextY);
+                }
+            }
 
-            graphics.DrawString(InnerText, innerFont, innerTextBrush, innerTextX, innerTextY);
 
+            float outerFontSize = Math.Max(this.Width, this.Height) * 0.06f;
+            using (Font outerFont = new Font("Arial", outerFontSize))
+            {
+                SizeF outerTextSize = graphics.MeasureString(OuterText, outerFont);
+                using (Brush outerTextBrush = new SolidBrush(Color.White))
+                {
+                    float outerTextX = outerCircleX + (outerCircleSize - outerTextSize.Width) / 2;
+                    float outerTextY = outerCircleY + outerCircleSize + outerCircleSize / 5; // Increase the offset
 
-            float outerFontSize = Math.Max(this.Width, this.Height) * 0.06f; // Adjust the multiplier for desired font size
-            Font outerFont = new Font("Arial", outerFontSize);
-            SizeF outerTextSize = graphics.MeasureString(OuterText, outerFont);
-            Brush outerTextBrush = new SolidBrush(Color.White);
-
-            float outerTextX = outerCircleX + (outerCircleSize - outerTextSize.Width) / 2;
-            float outerTextY = outerCircleY + outerCircleSize + outerCircleSize / 5; // Increase the offset
-
-            graphics.DrawString(OuterText, outerFont, outerTextBrush, outerTextX, outerTextY);
+                    graphics.DrawString(OuterText, outerFont, outerTextBrush, outerTextX, outerTextY);
+                }
+            }
         }
+
+        
 
         private void Leave_Event()
         {
-            this.BackColor = Color.FromArgb(32, 32, 32);
+            BackColor = Color.FromArgb(32, 32, 32);
             Elapsed = false;
 
             HideTimer.Start();
@@ -108,7 +115,7 @@ namespace WidgetsApp.src.controls
 
         private void Hover_Event()
         {
-            this.BackColor = Color.FromArgb(50, 50, 50);
+            BackColor = Color.FromArgb(50, 50, 50);
 
             if (HideTimer.Enabled)
             {
@@ -128,10 +135,9 @@ namespace WidgetsApp.src.controls
             }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void SettingsButton_Click(object sender, EventArgs e)
         {
-            // create dropdown menu
-            Console.WriteLine("Button clicked");
+            contextMenuStrip1.Show(SettingsButton, new Point(0, SettingsButton.Height));
         }
 
         private void ShowTimer_Tick(object sender, EventArgs e)
@@ -142,6 +148,27 @@ namespace WidgetsApp.src.controls
         private void HideTimer_Tick(object sender, EventArgs e)
         {
             SettingsButton.Hide();
+        }
+
+        private void editToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (Parent.Parent is MainForm mainForm)
+            {
+                mainForm.EditShortcut(this);
+            }
+        }
+
+        private void removeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (Parent.Parent is MainForm mainForm)
+            {
+                mainForm.RemoveShortcut(this);
+            }
+        }
+
+        public WidgetData GetWidgetData()
+        {
+            return new WidgetData(OuterText, InnerText);
         }
     }
 }
