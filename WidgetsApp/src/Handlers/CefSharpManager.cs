87347aab10bd9dbe6a745c24554476dd397ef89b
Bailey <53024171+baileydel@users.noreply.github.com>
2024-07-12 21:00:51 -0700

    Create CefSharpManager.cs

---
+++ WidgetsApp/src/Handlers/CefSharpManager.cs
@@ -0,0 +1,35 @@
+﻿
+using CefSharp;
+using CefSharp.WinForms;
+using WidgetsApp.src.Util;
+
+
+namespace WidgetsApp.src.Handlers
+{
+    public class CefSharpManager
+    {
+        public CefSharpManager()
+        {
+            
+        }
+
+        public void Initialize()
+        {
+            CefSettingsBase settings = new CefSettings
+            {
+                CachePath = FileManager.BROWSERPATH
+            };
+
+            settings.CefCommandLineArgs.Add("enable-persistent-cookies", "1");
+
+            CefSharpSettings.ConcurrentTaskExecution = true;
+            CefSharpSettings.ShutdownOnExit = true;
+            Cef.Initialize(settings);
+        }
+
+        public bool IsInitialized()
+        {
+            return Cef.IsInitialized;
+        }
+    }
+}
