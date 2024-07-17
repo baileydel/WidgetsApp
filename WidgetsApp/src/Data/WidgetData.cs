c189b6a9a5f45955d3082a40941fcd5ea4365875
Bailey <53024171+baileydel@users.noreply.github.com>
2024-07-17 13:39:33 -0700

    Refactored: Removed unused constructors

--- WidgetsApp/src/Data/WidgetData.cs
+++ WidgetsApp/src/Data/WidgetData.cs
@@ -11,9 +11,7 @@ namespace WidgetsApp
         public Size Size { get; set; }
         public string Url { get; set; }
         public Color Color { get; set; }
-
         public string SavePath { get; set; }
-
 
         [JsonConstructor]
         public WidgetData(string name, Size size, Point location, string url, Color color)
@@ -22,23 +20,6 @@ namespace WidgetsApp
             Size = size;
             Location = location;
             Url = url;
-            Color = color;
-        }
-
-        public WidgetData(Size size, Point location, string url)
-        {
-            Name = url; 
-            Size = size;
-            Location = location;
-            Url = url;
-        }
-
-        public WidgetData(string name, string url, Color color)
-        {
-            Name = name;
-            Url = url;
-            Size = new Size(526, 337);
-            Location = new Point(0, 0);
             Color = color;
         }
 
